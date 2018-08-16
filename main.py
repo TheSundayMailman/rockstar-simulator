fd99635b849e62c81220757069304fa420b966ff
Albert Sare <thesundaymailman@gmail.com>
2018-08-16 15:22:35 -0700

    refactored game logic out of rockstar class

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from rockstar import Rockstar
 
+
+
+def get_player_status(player):
+  print(" > Popularity: " + str(player.popularity) + "%")
+  print(" > Cash: $" + str(player.money))
+  print(" > Party cost: $" + str(player.cost))
+  print(" > Energy: " + str(player.energy) + "%\n")
+
+
+
 def rockstar_sim():
   # print("{} has ran.".format(rockstar_sim.__name__))
 
@@ -15,8 +25,7 @@ def rockstar_sim():
 
 Welcome to Rockstar Simulator 1.0!
 Play as a rockstar! Manage your career for 12 months!
-Be remembered as a rock legend! Or party too hard and die...
-  """
+Be remembered as a rock legend! Or party too hard and die..."""
 
   hint = """
 Every month, your rockstar can either 'tour', 'rest', or 'party'.
@@ -25,8 +34,7 @@ Every month, your rockstar can either 'tour', 'rest', or 'party'.
 > Partying uses cash, but can boost popularity and energy.
 
 Rockstars beware!
-Partying too hard while you are too popular might have devasting effects...
-  """
+Partying too hard while you are too popular might have devasting effects..."""
 
   divider = "\n==========================================================================="
 
@@ -46,28 +54,54 @@ Partying too hard while you are too popular might have devasting effects...
 
   print(hint)
   print(divider)
-  print("\nYour rockstar will named {}! May {} career be successful...".format(player.name, player.posessive))
-  player.show_status()
+  print("\nYour rockstar will named {}! May {} career be legendary...".format(player.name, player.posessive))
+  get_player_status(player)
 
   # start of game loop
   running = True
   months = 0
-
   while running and player.alive and months < 12:
     cmd = input("{} of 12 months have passed. What will {} do next?\nType 'help' for help, or 'quit' to exit.\n".format(months, player.name)).lower()
     if cmd == "quit" or cmd == "exit":
       running = False
     elif cmd == "tour":
-      print(divider)
-      player.tour()
+      if player.energy < 20:
+        print(divider)
+        print("\nOh no! {} has no energy to perform...so {} takes a rest instead...".format(player.name, player.pronoun))
+        player.fail_tour()
+      elif player.popularity < 100:
+        print(divider)
+        print("\n{} tours around the country to perform!!!".format(player.name))
+        print("Oh no...{} seems to be losing it with {} fans...".format(player.pronoun, player.posessive))
+        player.unpopular_tour()
+      else:
+        print(divider)
+        print("\n{} tours around the country to perform!!!".format(player.name))
+        player.tour()
+      get_player_status(player)
+      months += 1
+    elif cmd == "party":
+      if player.money < player.cost:
+        print(divider)
+        print("\n{} is too broke to party...".format(player.name))
+        print("So {} just sits and mopes around for awhile...".format(player.pronoun))
+        player.broke_party()
+      elif player.energy < 15:
+        print(divider)
+        print("\n{} does not have enough energy to party...".format(player.name))
+        print("So {} just sits and mopes around for awhile...".format(player.pronoun))
+        player.fail_party()
+      else:
+        print(divider)
+        print("\n{} parties like a rockstar!!!".format(player.name))
+        player.party()
+      get_player_status(player)
       months += 1
     elif cmd == "rest":
       print(divider)
+      print("\n{} goes on a haitus...".format(player.name))
       player.rest()
-      months += 1
-    elif cmd == "party":
-      print(divider)
-      player.party()
+      get_player_status(player)
       months += 1
     elif cmd == "help":
       print(divider)
@@ -76,8 +110,27 @@ Partying too hard while you are too popular might have devasting effects...
       print(divider)
       print("\nPlease type in a valid command...\n")
 
+  # check reason for loop exit
+  if player.alive == False:
+    print("{} has died from partying too hard...".format(player.name))
+    if player.popularity < 50:
+      print("And {} died with little fans to remember {}, sad...".format(player.pronoun, player.posessive))
+  
+  # evaluate result
+  print(divider)
+  print("After one year of being a rockstar,")
+  if player.popularity <= 50:
+    print("{} is not very popular...".format(player.name))
+  elif player.popularity > 50 and player.popularity <= 120:
+    print("{} is somewhat popular...".format(player.name))
+  else:
+    print("{} is very popular!".format(player.name))
 
 
+
+# main function call
 rockstar_sim()
 
+
+# exit greeting
 print("\nThanks for playing!\n")
--- rockstar.py
+++ rockstar.py
@@ -12,69 +12,63 @@ class Rockstar():
     self.energy = 100
     self.cost = 500
 
+  # update cost of next party, called after every method
   def update_cost(self):
     self.cost = int(self.popularity * random.randint(4, 5))
 
-  def show_status(self):
-    # status = ("dead", "alive")[self.alive]
-    print(" > Popularity: " + str(self.popularity) + "%")
-    print(" > Cash: $" + str(self.money))
-    print(" > Party cost: $" + str(self.cost))
-    print(" > Energy: " + str(self.energy) + "%\n")
-
+  # all tour methods
   def tour(self):
-    if self.energy == 0:
-      print("\nOh no! {} has no energy to perform...so {} takes a rest instead...".format(self.name, self.pronoun))
-      self.energy = 10
-      self.popularity -= random.randint(25, 45)
-      self.money += random.randint(1, 3) * self.popularity
-    else:
-      print("\n{} tours around the country to perform!!!".format(self.name))
-      if self.popularity < 100:
-        print("Oh no...{} seems to be losing it with {} fans...".format(self.pronoun, self.posessive))
-        self.energy -= random.randint(30, 50)
-        self.popularity += random.randint(0, 10)
-        self.money += random.randint(1, 5) * self.popularity
-      else:
-        self.energy -= random.randint(25, 45)
-        self.popularity += random.randint(30, 50)
-        self.money += random.randint(5, 10) * self.popularity
-    if self.energy < 0:
-      self.energy = 0
-    if self.popularity < 0:
-      self.popularity = 0
-    self.update_cost()
-    self.show_status()
-
-  def party(self):
-    if self.money < self.cost:
-      print("\n{} is too broke to party...".format(self.name))
-      print("So {} just sits and mopes around for awhile...".format(self.pronoun))
-      self.energy += random.randint(1, 5)
-      self.popularity -= int(random.randint(30, 50) * self.popularity / 100)
-    elif self.energy < 15:
-      print("\n{} does not have enough energy to party...".format(self.name))
-      print("So {} just sits and mopes around for awhile...".format(self.pronoun))
-      self.energy += random.randint(1, 5)
-      self.popularity -= int(random.randint(30, 50) * self.popularity / 100)
-    else:
-      print("\n{} parties like a rockstar!!!".format(self.name))
-      self.energy -= random.randint(12, 18)
-      self.popularity += random.randint(20, 40)
-      self.money -= self.cost
-    if self.popularity < 0:
-      self.popularity = 0
+    self.energy -= random.randint(25, 45)
+    self.popularity += random.randint(30, 50)
+    self.money += random.randint(5, 10) * self.popularity
     if self.energy < 0:
       self.energy = 0
     self.update_cost()
-    self.show_status()
 
-  def rest(self):
-    print("\n{} goes on a haitus...".format(self.name))
-    self.energy += random.randint(25, 40)
+  def unpopular_tour(self):
+    self.energy -= random.randint(25, 45)
+    self.popularity += random.randint(0, 10)
+    self.money += random.randint(1, 5) * self.popularity
+    if self.energy < 0:
+      self.energy = 0
+    self.update_cost()
+
+  def fail_tour(self):
+    self.energy += random.randint(6, 12)
     self.popularity -= random.randint(25, 45)
+    self.money += random.randint(1, 3) * self.popularity
+    if self.popularity < 0:
+      self.popularity = 0
+    self.update_cost()
+
+  # all party methods
+  def party(self):
+    self.energy -= random.randint(12, 18)
+    self.popularity += random.randint(30, 50)
+    self.money -= self.cost
+    if self.energy < 0:
+      self.energy = 0
+    self.update_cost()
+
+  def broke_party(self):
+    self.energy += random.randint(1, 5)
+    self.popularity -= int(random.randint(30, 50) * self.popularity / 100)
+    if self.popularity < 0:
+      self.popularity = 0
+    self.update_cost()
+
+  def fail_party(self):
+    self.energy += random.randint(1, 5)
+    self.popularity -= int(random.randint(30, 50) * self.popularity / 100)
+    if self.popularity < 0:
+      self.popularity = 0
+    self.update_cost()
+
+  # all rest methods
+  def rest(self):
+    self.energy += random.randint(25, 40)
+    self.popularity -= random.randint(20, 30)
     self.money += random.randint(0, 10)
     if self.popularity < 0:
       self.popularity = 0
     self.update_cost()
-    self.show_status()
